fdf767d1f5c64f189b20e70fad44a768ba8ce561
AlexanderGabor <gaboralexanderalex@gmail.com>
2024-11-20 14:48:08 +0100

    feat:add DogController

---
+++ backend/src/main/java/salt/alexander/petpals/controller/DogController.java
@@ -0,0 +1,21 @@
+package salt.alexander.petpals.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import salt.alexander.petpals.model.Dog;
+import salt.alexander.petpals.service.DogService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/dogs")
+public class DogController {
+
+    @Autowired
+    private DogService dogService;
+
+    @GetMapping
+    public List<Dog> getAllDogs() {
+        return dogService.getAllDogs(); // Return all dogs
+    }
+}
